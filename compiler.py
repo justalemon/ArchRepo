f1e58ca704d3cf2e9e8d57c9506a23ff62218798
Hannele Ruiz <justlemoncl@gmail.com>
2025-12-03 03:41:59 -0300

    Added function to get the list of packages to compile

--- compiler.py
+++ compiler.py
@@ -1,8 +1,18 @@
+from pathlib import Path
+
 import typer
+import yaml
+
+
+def get_list_of_packages():
+    path = Path.cwd() / "packages.yml"
+    contents = path.read_text(encoding="utf-8")
+    parsed = yaml.load(contents, Loader=yaml.Loader)
+    return parsed["packages"]
 
 
 def main():
-    pass
+    packages = get_list_of_packages()
 
 
 if __name__ == "__main__":

ce50e8ea5112b84678fb40dea99e77c52aeb51ff
Hannele Ruiz <justlemoncl@gmail.com>
2025-12-04 03:14:23 -0300

    Added basic build process

--- compiler.py
+++ compiler.py
@@ -1,7 +1,11 @@
+import sys
 from pathlib import Path
 
+import docker
 import typer
 import yaml
+from colorama import Fore, Style
+from docker.errors import ImageNotFound
 
 
 def get_list_of_packages():
@@ -11,8 +15,22 @@ def get_list_of_packages():
     return parsed["packages"]
 
 
-def main():
-    packages = get_list_of_packages()
+def main(build: bool = False):
+    docker_client = docker.from_env()
+
+    if build:
+        print(f"{Fore.WHITE}Building docker image as {Fore.MAGENTA}archbuilder{Fore.WHITE}, please wait...{Style.RESET_ALL}")
+        image, _ = docker_client.images.build(path=str(Path.cwd()), tag="archbuilder", rm=True, nocache=True)
+    else:
+        try:
+            image = docker_client.images.get("archbuilder")
+            print(f"{Fore.WHITE}Using existing {Fore.MAGENTA}archbuilder{Fore.WHITE} image{Style.RESET_ALL}")
+        except ImageNotFound:
+            sys.exit("Could not find archbuilder image, use --build to build it.")
+
+    for package in get_list_of_packages():
+        print(f"{Fore.WHITE}Building package {Fore.MAGENTA}{package}{Fore.WHITE}...{Style.RESET_ALL}")
+        docker_client.containers.run(image, f"/home/builder/build.sh {package}", True)
 
 
 if __name__ == "__main__":

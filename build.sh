54b3d3c7873932362a301bc0eeec7a7d574adba1
Hannele Ruiz <justlemoncl@gmail.com>
2025-12-03 21:55:12 -0300

    Added script to build a package quickly

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+rm -rf build
+git clone "https://aur.archlinux.org/$1.git" build
+cd build || exit 1
+makepkg -sf --noconfirm
+rm -rf ~/pkg
+mkdir ~/pkg
+cp -v *.{pkg.tar.zst,tar.gz} ~/pkg

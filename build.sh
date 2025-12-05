ade84164136f1dfc84ea84d0acec2e55fcd45a77
Hannele Ruiz <justlemoncl@gmail.com>
2025-12-05 04:48:14 -0300

    Copy the packages and source to the pkg folder

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ git clone "https://aur.archlinux.org/$1.git" build
 cd build || exit 1
 makepkg -sf --noconfirm
 mkdir ~/pkg || true
-cp -v *.{pkg.tar.zst} ~/pkg
+cp -v *.pkg.tar.zst ~/pkg
+cp -v *.tar.gz ~/pkg

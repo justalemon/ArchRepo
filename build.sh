f958ef411c9e9e1e74deaa14b08ef387ce7d7366
Hannele Ruiz <justlemoncl@gmail.com>
2025-12-05 04:45:26 -0300

    Make the directory creation optional
    Might be mounted externally

--- build.sh
+++ build.sh
@@ -5,6 +5,5 @@ rm -rf build
 git clone "https://aur.archlinux.org/$1.git" build
 cd build || exit 1
 makepkg -sf --noconfirm
-rm -rf ~/pkg
-mkdir ~/pkg
+mkdir ~/pkg || true
 cp -v *.{pkg.tar.zst} ~/pkg
